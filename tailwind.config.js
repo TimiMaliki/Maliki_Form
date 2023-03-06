3a85f84564b92ed07c3d55d07383a639b0fae808
TimiMaliki <malikitimi@gmail.com>
2023-03-06 11:18:36 -0800

    done

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'background': '#e8e8f3',
+        'background-n': '#dfe6e9',
+        'box': '#b2bec3',
+        'secondary-blue': '#5DE6DE',
+        'primary-purple': '#9E768F',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+     
+   
+    
+    },
+  },
+  plugins: [],
+}
